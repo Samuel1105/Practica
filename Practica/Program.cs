fe2d8b60739079e2037158d333afde8c037b8daf
Your Name <you@example.com>
2022-02-11 10:36:09 -0400

    Agregar archivos de proyecto.

---
+++ Practica/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! s");
+            
+        }
+    }
+}
