a867ae14714a52de457f6cc20f9a575b10b92621
Your Name <you@example.com>
2022-02-11 11:14:18 -0400

    sa

---
+++ Practica/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practica
+{
+    class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
--- Practica/Program.cs
+++ Practica/Program.cs
@@ -6,8 +6,29 @@ namespace Practica
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! s");
             
+            Console.WriteLine("Cuantas Personas son:");
+            int dato;
+            dato = int.Parse(Console.ReadLine());
+            string[] alumno = new string[dato];
+            Persona[] P = new Persona[dato]; ; 
+            for (int c = 0; c < dato; c++)
+            {
+                Console.WriteLine("Ingrese Nombre del Estudiante N° " + (c + 1));
+                P[c].Nombre = Console.ReadLine().ToString();
+                Console.WriteLine("Ingrese Edad del Estudiante N° " + (c + 1));
+                P[c].Edad = int.Parse(Console.ReadLine());
+
+               
+            }
+            for (int c = 0; c < dato; c++)
+            {
+                Console.Write(P[c].Nombre);
+                Console.Write(" ");
+                Console.WriteLine(P[c].Edad);
+            }
+            
+
         }
     }
 }
